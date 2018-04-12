f0aa6abb3d6973cb49565ff57ad1677710e706d6
nanana3 <g16920kn@s30403.tsuda.ac.jp>
2018-04-12 14:24:01 +0900

    add new file

---
+++ Sample2.java
@@ -0,0 +1,12 @@
+package kadai1;
+
+public class Sample2 {
+
+	public static void main(String[] args){
+		int n = 100;
+		for(int i=0; i<10; i++){
+			n = n + i;
+		}
+		System.out.println(n);
+	}
+}
